f4f641f1106fc81d7de6cdadf185dfa80ffe7ce5
vivitruong <vivitruong1506@gmail.com>
2023-12-08 12:02:49 -0600

    varibales lessions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Java-self-taught
+### D
---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+  public static void main(String[] args) {
+    int dogCount = 94;
+    int catCount = 112;
+    int petCount = dogCount + catCount;
+    System.out.println(petCount);
+  }
+}
