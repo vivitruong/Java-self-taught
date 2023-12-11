bb40dba51ececf6a4acc414a60ae955bd5b45096
vivitruong <vivitruong1506@gmail.com>
2023-12-10 19:48:21 -0600

    learn more about string in java

---
+++ READMEstrings.md
@@ -0,0 +1,2 @@
+## Strings
+String is a Java reference type. Reference types differ from primitive types in the following ways.
---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        String message;                // 1. Declare a variable with the String type.
+        message = "My dog has fleas."; // 2. Assign a literal string value to the variable `message`.
+        System.out.println(message);   // 3. Print the contents of `message` to the console.
+
+
+
+
+    }
+}
