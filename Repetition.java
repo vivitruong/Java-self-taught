d3568932f992789cbfae641ac724395b3c25c67f
vivitruong <vivitruong1506@gmail.com>
2023-12-17 21:58:01 -0600

    repetion

---
+++ Repetition.java
@@ -0,0 +1,9 @@
+
+public class Repetition {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("this message is repeated forever");
+        }
+
+    }
+}
