28926a965861c72cebbb1efb44daac1e918a5c5d
vivitruong <vivitruong1506@gmail.com>
2023-12-17 22:03:01 -0600

    Repitetion

--- Repetition.java
+++ Repetition.java
@@ -1,9 +1,17 @@
+import java.util.Random;
 
 public class Repetition {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("this message is repeated forever");
-        }
+        Random rand = new Random();
+        int a = 1;
+        int b = 2;
+        int c = 3;
 
+        while (a < 50 || b % 2 == 0 && c % 3 != 0){
+            System.out.printf("%s,%s,%s%n", a,b,c);
+            a = rand.nextInt(100);
+            b = rand.nextInt(100);
+            c = rand.nextInt(100);
+        }
     }
 }
