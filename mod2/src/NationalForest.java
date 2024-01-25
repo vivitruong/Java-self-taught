38cdd455ae215210d14b11f07190491e618f53a1
vivitruong <vivitruong1506@gmail.com>
2024-01-24 18:49:18 -0600

    classes and method

--- mod2/src/NationalForest.java
+++ mod2/src/NationalForest.java
@@ -22,4 +22,13 @@ public class NationalForest {
         this.location = location;
         this.acres = acres;
     }
+
+    public int getSquareKilometers() {
+        return (int) (this.acres / 247.1);
+    }
+
+    public String toLine() {
+        return String.format("name: %s, location: %s, acres: %s, km^2: %s", name, location, acres,
+        getSquareKilometers());
+    }
 }
