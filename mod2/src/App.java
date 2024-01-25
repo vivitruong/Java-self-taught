027bb6e2ef34e02664529a561b8b555d25cfdd3f
vivitruong <vivitruong1506@gmail.com>
2024-01-24 18:46:11 -0600

    begibning of mod 2

--- mod2/src/App.java
+++ mod2/src/App.java
@@ -3,6 +3,15 @@ package src;
 
 public class App {
     public static void main(String[] args){
+        NationalForest one = new NationalForest("Allegheny", "Pennsylvania", 513655);
+        NationalForest two = new NationalForest("Angeles", "California", 661565);
+        NationalForest three = new NationalForest("Angelina", "Texas", 154140);
+        NationalForest four = new NationalForest();
+        String format = "name: %s, location %s, arces: %s%n";
 
+        System.out.printf(format, one.name, one.location, one.acres);
+        System.out.printf(format, two.name, two.location, two.acres);
+        System.out.printf(format, three.name, three.location, three.acres);
+        System.out.printf(format, four.name, four.location, four.acres);
     }
 }
--- mod2/src/NationalForest.java
+++ mod2/src/NationalForest.java
@@ -5,4 +5,21 @@ public class NationalForest {
     public String name;
     public String location;
     public int acres;
+
+    public NationalForest(){
+        this("Unknown", "Unknown", -1);
+    }
+
+    public NationalForest(String name) {
+        this(name, "Unknown", -1);
+    }
+
+    public NationalForest(String name, int acres) {
+        this(name, "Unknown", acres);
+    }
+    public NationalForest(String name, String location, int acres){
+        this.name = name;
+        this.location = location;
+        this.acres = acres;
+    }
 }
